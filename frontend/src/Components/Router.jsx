c42fc7ab004f24e2469065fd79c6b978b856eb2e
Hasan Zalkout <hasan.zalkout@gmail.com>
2023-03-31 01:32:55 -0500

    individual truck page when selected

--- frontend/src/Components/Router.jsx
+++ frontend/src/Components/Router.jsx
@@ -6,6 +6,7 @@ import Profile from "../Views/Profile";
 import SignUp from "../Views/SignUp";
 import TruckRental from "../Views/TruckRental";
 import ResponsiveNavbar from "./Navbar";
+import TruckRentalDetails from "../Views/TruckRentalDetails";
 
 export const Router = () => (
   <BrowserRouter>
@@ -17,6 +18,7 @@ export const Router = () => (
       <Route path="homepage" element={<Homepage />} />
       <Route path="profile" element={<Profile />} />
       <Route path="truckrental" element={<TruckRental />} />
+      <Route path="truckrentaldetails" element={<TruckRentalDetails />} />
       <Route path="passwordreset" element={<PasswordReset />} />
     </Routes>
   </BrowserRouter>
--- frontend/src/Views/TruckRental.jsx
+++ frontend/src/Views/TruckRental.jsx
@@ -1,16 +1,17 @@
-  import React, { useContext, useEffect, useState } from "react";
-  import Button from "@mui/material/Button";
-  import CssBaseline from "@mui/material/CssBaseline";
-  import TextField from "@mui/material/TextField";
-  import Box from "@mui/material/Box";
-  import Typography from "@mui/material/Typography";
-  import Container from "@mui/material/Container";
-  import { createTheme, ThemeProvider } from "@mui/material/styles";
-  import { InputLabel, MenuItem, Select } from "@mui/material";
-  import { addUser } from "../API/Api";
-  import { user } from "../Models/user";
-  import { AppContext } from "../Context/AppContext";
-  import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { InputLabel, MenuItem, Select } from "@mui/material";
+import { addUser } from "../API/Api";
+import { user } from "../Models/user";
+import { AppContext } from "../Context/AppContext";
+import { useNavigate } from "react-router-dom";
+import TruckRentalDetails from "./TruckRentalDetails";
   
   const theme = createTheme();
   
@@ -51,69 +52,80 @@
         specifications: "Specification 1, Specification 2, Specification 3",
       },
   ];
-  
-  export default function TruckRental() {
-    const [selectedTruck, setSelectedTruck] = useState(null);
-  
-    const handleSelectTruck = (truck) => {
-      setSelectedTruck(truck);
-    };
-  
-    return (
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="md">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Truck Rental
-            </Typography>
-            <Box sx={{ width: '100%' }}>
-              {trucks.map((truck) => (
-                <Box
-                  key={truck.id}
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    p: 2,
-                    borderBottom: "1px solid #ccc",
-                  }}
-                >
-                  <Box sx={{ mr: 2 }}>
-                    <img src={truck.image} alt={truck.name} width="100" />
-                  </Box>
-                  <Box>
-                    <Typography component="h2" variant="h6">
-                      {truck.name}
-                    </Typography>
-                    <Typography component="p" variant="body1">
-                      {truck.description}
-                    </Typography>
-                    <Typography component="p" variant="body2">
-                      Specifications: {truck.specifications}
-                    </Typography>
-                    <Button
-                      variant="contained"
-                      onClick={() => handleSelectTruck(truck)}
-                      sx={{ mt: 2 }}
-                    >
-                      Select
-                    </Button>
-                  </Box>
+
+
+
+const TruckRental = () => {
+  const [selectedTruck, setSelectedTruck] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSelectTruck = (truck) => {
+    setSelectedTruck(truck);
+    navigate("/truckrentaldetails", { state: { truck } });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="md">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Truck Rental
+          </Typography>
+          <Box sx={{ width: '100%' }}>
+            {trucks.map((truck) => (
+              <Box
+                key={truck.id}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  p: 2,
+                  borderBottom: "1px solid #ccc",
+                }}
+              >
+                <Box sx={{ mr: 2 }}>
+                  <img src={truck.image} alt={truck.name} width="100" />
                 </Box>
-              ))}
-            </Box>
+                <Box>
+                  <Typography component="h2" variant="h6">
+                    {truck.name}
+                  </Typography>
+                  <Typography component="p" variant="body1">
+                    {truck.description}
+                  </Typography>
+                  <Typography component="p" variant="body2">
+                    Specifications: {truck.specifications}
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    onClick={() => handleSelectTruck(truck)}
+                    sx={{ mt: 2, mr: 2 }}
+                  >
+                    Select
+                  </Button>
+                </Box>
+              </Box>
+            ))}
           </Box>
-        </Container>
-      </ThemeProvider>
-    );
-  }
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default TruckRental;
+
+
+  
+
+
   
   
   
