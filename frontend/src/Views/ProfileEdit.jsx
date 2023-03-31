80f7ef5ef3a5b1e92aa576a91c64b1fdae001d39
bwyma1 <brockdwyma@gmail.com>
2023-03-31 15:57:36 -0500

    fixed error

--- frontend/src/Views/ProfileEdit.jsx
+++ frontend/src/Views/ProfileEdit.jsx
@@ -4,63 +4,66 @@ import { getUserByEmail } from "../API/Api";
 import { TextField } from "@mui/material";
 
 export default function ProfileEdit() {
-    const user = JSON.parse(window.sessionStorage.getItem("user"));
+  const user = JSON.parse(window.sessionStorage.getItem("user"));
 
-    const navigate = useNavigate();
-    const backButton = () => {
-        navigate(`/profile`);
-    };
+  const navigate = useNavigate();
+  const backButton = () => {
+    navigate(`/profile`);
+  };
 
-    let event = null;
-    const data = new FormData(event.currentTarget);
-    const confirmButton = () => {
-        let newuser = new user(
-            user.email,
-            user.user_name,
-            data.get("password") || user.password,
-            user.security_question,
-            user.security_question_answer,
-            user.user_id,
-            data.get("bio") || user.bio,
-            data.get("location") || user.location,
-            data.get("phone") || user.phone_number,
-            data.get("profilepicture") || user.profile_picture
-          )
-        navigate(`/profile`);
-    };
+  let event = null;
+  const data = new FormData(event.currentTarget);
+  const confirmButton = () => {
+    let newuser = new user(
+      user.email,
+      user.user_name,
+      data.get("password") || user.password,
+      user.security_question,
+      user.security_question_answer,
+      user.user_id,
+      data.get("bio") || user.bio,
+      data.get("location") || user.location,
+      data.get("phone") || user.phone_number,
+      data.get("profilepicture") || user.profile_picture
+    );
+    navigate(`/profile`);
+  };
 
-    return<><br></br>
-    <button onClick={backButton}>Back</button>
-    <h2>Edit Profile</h2>
-    <TextField
+  return (
+    <>
+      <br></br>
+      <button onClick={backButton}>Back</button>
+      <h2>Edit Profile</h2>
+      <TextField
         margin="normal"
         fullWidth
         label="Bio"
         name="bio"
         value={user.bio}
-    />
-    <TextField
+      />
+      <TextField
         margin="normal"
         fullWidth
         label="Location"
         name="location"
         value={user.location}
-    />
-    <TextField
+      />
+      <TextField
         margin="normal"
         fullWidth
         name="password"
         label="Password"
         type="password"
         defaultValue={user.password}
-    />
-    <TextField
+      />
+      <TextField
         margin="normal"
         fullWidth
         label="Phone Number"
         name="phone"
         value={user.phone_number}
-    />
-    <button onClick={confirmButton}>Confirm</button>
-    </>;
+      />
+      <button onClick={confirmButton}>Confirm</button>
+    </>
+  );
 }
