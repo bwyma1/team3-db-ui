c2ded3b0af6f5f526c8db5e8f3a90f1883998550
Ethan Zech <ezech@smu.edu>
2023-04-11 13:33:01 -0500

    Changed Vehicle Bundle Tables And Wrote Vehicle Bundle Backend Calls

--- DBUI.session.sql
+++ DBUI.session.sql
@@ -69,9 +69,11 @@ CREATE TABLE IF NOT EXISTS vehicle_bundle_profile(
     FOREIGN KEY (owner_id) REFERENCES user(user_id)
 );
 CREATE TABLE IF NOT EXISTS bundle_vehicle(
-    bundle_id INT AUTO_INCREMENT PRIMARY KEY,
+    vehicle_bundle_id INT AUTO_INCREMENT PRIMARY KEY,
+    bundle_id INT NOT NULL,
     truck_id INT NOT NULL,
-    FOREIGN KEY (truck_id) REFERENCES truck(truck_id)
+    FOREIGN KEY (truck_id) REFERENCES truck(truck_id),
+    FOREIGN KEY (bundle_id) REFERENCES vehicle_bundle_profile(bundle_id)
 );
 CREATE TABLE IF NOT EXISTS amenity(
     amenity_id INT AUTO_INCREMENT PRIMARY KEY,
--- backend/server.js
+++ backend/server.js
@@ -59,6 +59,7 @@ app.post('/user', (req, res) => {
     })
 })
 
+// Creates a truck and attaches it to a user by their email
 app.post('/truck', (req, res) => {
     const {email, model, make, year, mileage, max_miles, long_discount_days, long_discount_percent, long_discount_flat, 
         truck_image} = req.body
@@ -71,6 +72,34 @@ app.post('/truck', (req, res) => {
             res.status(200)
             res.send(true)
         })
+})
+
+// Creates a vehicle bundle profile connected to a users email
+app.post('/vehicle_bundle', (req, res) => {
+    const {email, discount_percent, discount_flat} = req.body
+    const query = `INSERT INTO vehicle_bundle_profile (owner_id, discount_percent, discount_flat)
+        VALUES ( (SELECT user_id AS this_user FROM user WHERE '${email}' = email), '${discount_percent}', '${discount_flat}')`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(rows)
+        res.status(200)
+        res.send(true)
+    })
+})
+
+// Adds a vehicle to a bundle given the vehicle and bundle id
+app.post('/vehicle_to_bundle', (req, res) => {
+    const {bundle_id, truck_id} = req.body
+    const query = `INSERT INTO bundle_vehicle (bundle_id, truck_id)
+        VALUES ('${bundle_id}', '${truck_id}')`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        console.log(rows)
+        res.status(200)
+        res.send(true)
+    })
 })
 
 app.get('/users', (req, res) => {
@@ -98,6 +127,30 @@ app.get('/user', (req, res) => {
 app.get('/user_trucks', (req, res) => {
     const email = req.query.email
     const query = `SELECT * FROM truck a WHERE a.owner_id = (SELECT b.user_id FROM user b WHERE b.email = '${email}');`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        res.status(200)
+        res.send(rows)
+    })
+})
+
+// get the bundles of a specific user with '/user_bundles?email='someEmail'
+app.get('/user_bundles', (req, res) => {
+    const email = req.query.email
+    const query = `SELECT * FROM vehicle_bundle_profile a WHERE a.owner_id = (SELECT b.user_id FROM user b WHERE b.email = '${email}');`
+    connection.query(query, (err, rows, fields) => {
+        if (err) throw err
+
+        res.status(200)
+        res.send(rows)
+    })
+})
+
+// gets all the trucks in the given bundle id
+app.get('/bundle_trucks', (req, res) => {
+    const bundle_id = req.query.bundle_id
+    const query = `SELECT * FROM bundle_vehicle a RIGHT JOIN truck b ON a.truck_id = b.truck_id WHERE a.bundle_id = '${bundle_id}'  `
     connection.query(query, (err, rows, fields) => {
         if (err) throw err
 
