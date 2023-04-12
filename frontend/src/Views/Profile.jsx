e8731e41dba0274398ca42ec4de63522cac64587
Cohen Smith <cohens@mail.smu.edu>
2023-04-12 17:16:33 -0500

    truck display
    displays user trucks in profile page

--- frontend/src/Views/Profile.jsx
+++ frontend/src/Views/Profile.jsx
@@ -1,23 +1,43 @@
-import { Navigate, useNavigate } from "react-router-dom";
-import React, { useContext, useMemo } from "react";
-import { getUserByEmail } from "../API/Api";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { getTrucksByEmail, getUserByEmail } from "../API/Api";
 
 export default function Profile() {
-  const user = JSON.parse(window.sessionStorage.getItem("user"));
+  const [user, setUser] = useState({})
+  const [response, setResponse] = useState({});
+  //const [truckResponse, setTruckResponse] = useState({});
+  const [truckList, setTruckList] = useState([{}]);
 
-  
-  function getUser() {
+  useEffect(() => {
+    setUser(JSON.parse(window.sessionStorage.getItem("user")))
+  }, [])
+
+  useEffect(() => {
     (async () => {
-      const response = await getUserByEmail(user.email);
-      
-      if (response) {
-        console.log(response);
-        return response;
+      const res = await getUserByEmail(user.email);
+      if (res) {
+        //console.log(user);
+        console.log(res);
+        setResponse(res);
       } else {
-        alert("error invalid email");
+        //alert("error invalid email");
       }
     })();
-  }
+  }, [user.email])
+
+  useEffect(() => {
+    (async () => {
+      const res = await getTrucksByEmail(user.email);
+      if (res) {
+        //console.log(user);
+        console.log(res);
+        //setTruckResponse(res);
+        setTruckList([res]);
+      } else {
+        //alert("error invalid email");
+      }
+    })();
+  }, [user.email])
 
   const navigate = useNavigate();
   const navigateToPage = () => {
@@ -27,12 +47,35 @@ export default function Profile() {
 
   return <>
   <header>This is the profile page</header>
-  <p>Username: {user.user_name}</p>
-  <p>Email: {user.email}</p>
+  <p>Username: {user.user_name ?? ""}</p>
+  <p>Email: {user.email ?? ""}</p>
   <p>Security Question: {user.security_question}</p>
-  <p>User Id: {getUser() ? getUser().user_id : "undefined"}</p>
-  <p>Location: {user.location}</p>
-  <p>Bio: {user.bio}</p>
+  <p>User Id: {response.user_id}</p>
+  <p>Location: {response.location ? response.location : "none"}</p>
+  <p>Bio: {response.bio ? response.bio : "none"}</p>
   <button onClick={navigateToPage}>Edit Profile</button>
+  <h2>Your Trucks:</h2>
+  {truckList.map((truck, index) => (
+    <div key={index}>
+      <h3>
+      {truck.year} {truck.model}
+      </h3>
+      <p>Mileage: {truck.mileage}</p>
+      <p>Max Miles: {truck.max_miles}</p>
+      <p>Long Discount Days: {truck.long_discount_days}</p>
+      <p>Long Discount Flat: {truck.long_discount_flat}</p>
+      <p>Long Discount Percent: {truck.long_discount_percent}</p>
+    </div>
+  ))}
   </>;
 }
+
+/*
+{truckList.map((truck, index) => (
+    <div>
+      <h1>
+      {index}
+      </h1>
+    </div>
+  ))}
+*/
--- frontend/src/Views/ProfileEdit.jsx
+++ frontend/src/Views/ProfileEdit.jsx
@@ -1,35 +1,87 @@
-import { Navigate, useNavigate } from "react-router-dom";
-import React, { useContext, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import React, { useState, useContext, useEffect } from "react";
 import { getUserByEmail } from "../API/Api";
 import { TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import { AppContext } from "../Context/AppContext";
 
 export default function ProfileEdit() {
-    const user = JSON.parse(window.sessionStorage.getItem("user"));
+    const currUser = JSON.parse(window.sessionStorage.getItem("user"));
+    const context = useContext(AppContext);
+
+    const [bio, setBio] = useState("");
+    const [location, setLocation] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const bioChange = (event) => setBio(event.target.value);
+    const locationChange = (event) => setLocation(event.target.value);
+    const phoneChange = (event) => setPhone(event.target.value);
 
     const navigate = useNavigate();
     const backButton = () => {
         navigate(`/profile`);
     };
 
-    let event = null;
-    const data = new FormData(event.currentTarget);
+    const [user, setUser] = useState({})
+  const [response, setResponse] = useState({});
+
+  useEffect(() => {
+    setUser(JSON.parse(window.sessionStorage.getItem("user")))
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+      const res = await getUserByEmail(user.email);
+      if (res) {
+        //console.log(user);
+        console.log(res);
+        setResponse(res);
+      } else {
+        //alert("error invalid email");
+      }
+    })();
+  }, [user.email])
+
     const confirmButton = () => {
         let newuser = new user(
             user.email,
             user.user_name,
-            data.get("password") || user.password,
+            user.password,
             user.security_question,
             user.security_question_answer,
             user.user_id,
-            data.get("bio") || user.bio,
-            data.get("location") || user.location,
-            data.get("phone") || user.phone_number,
-            data.get("profilepicture") || user.profile_picture
+            bio || user.bio,
+            location || user.location,
+            phone || user.phone_number,
+            user.profile_picture
+          );
+          console.log(newuser);
+          context.setCurrUser(new user(
+            user.email,
+            user.user_name,
+            user.password,
+            user.security_question,
+            user.security_question_answer,
+            user.user_id,
+            bio || user.bio,
+            location || user.location,
+            phone || user.phone_number,
+            user.profile_picture
           )
+          );
+          //*/
         navigate(`/profile`);
     };
 
     return<><br></br>
+    <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
     <button onClick={backButton}>Back</button>
     <h2>Edit Profile</h2>
     <TextField
@@ -37,30 +89,26 @@ export default function ProfileEdit() {
         fullWidth
         label="Bio"
         name="bio"
-        value={user.bio}
+        value={response.user_id}
+        onChange={bioChange}
     />
     <TextField
         margin="normal"
         fullWidth
         label="Location"
         name="location"
-        value={user.location}
-    />
-    <TextField
-        margin="normal"
-        fullWidth
-        name="password"
-        label="Password"
-        type="password"
-        defaultValue={user.password}
+        value={currUser.location}
+        onChange={locationChange}
     />
     <TextField
         margin="normal"
         fullWidth
         label="Phone Number"
         name="phone"
-        value={user.phone_number}
+        value={currUser.phone_number}
+        onChange={phoneChange}
     />
     <button onClick={confirmButton}>Confirm</button>
+    </Box>
     </>;
 }
