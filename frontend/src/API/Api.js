17a148c15377eb3f703b8bd4dbfd2beab1819f17
bwyma1 <brockdwyma@gmail.com>
2023-04-19 17:11:21 -0500

    added truck,message api functions

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -87,6 +87,54 @@ export const updatePassword = async (email, password) => {
     return response.data
 }
 
+export const getTruckReviews = async (truck_id) => {
+  let response
+  try {
+    response = await axios.get(
+    url + `/truck_review?truck_id=${truck_id}`
+  )} catch (err) {
+    console.log(err)
+  }
+  return response.data
+}
+
+export const addTruckReview = async (user_id,truck_id,review_text,review_rating) => {
+  const data = {user_id:user_id, review_text:`${review_text}`,review_rating:`${review_rating}`}
+  let response
+  try {
+    response = await axios.post(
+    url + `/truck_review?truck_id=${truck_id}`, data
+  )} catch (err) {
+    console.log(err)
+  }
+  return response.data
+}
+
+export const addTruck = async (email, model, make, year, mileage, max_miles, long_discount_days, long_discount_percent, long_discount_flat,truck_image) => {
+  const data = {email:`${email}`, model:`${model}`, make:`${make}`, year:`${year}`, mileage:`${mileage}`, max_miles:max_miles, long_discount_days:long_discount_days, 
+    long_discount_percent:long_discount_percent, long_discount_flat:long_discount_flat,truck_image:`${truck_image}`}
+  let response
+  try {
+    response = await axios.post(
+    url + `/truck`, data
+  )} catch (err) {
+    console.log(err)
+  }
+  return response.data
+}
+
+export const createVehicleBundle = async (email,discount_percent,discount_flat) => {
+  const data = {email:`${email}`, discount_percent:discount_percent,discount_flat:discount_flat}
+  let response
+  try {
+    response = await axios.post(
+    url + `/vehicle_bundle`, data
+  )} catch (err) {
+    console.log(err)
+  }
+  return response.data
+}
+
 export const updateProfile = async (email, bio, prof_pic_choice, location, phone) => {
   const data = {bio:`${bio}`, prof_pic_choice: prof_pic_choice, location:`${location}`, phone:`${phone}`}
   let response
