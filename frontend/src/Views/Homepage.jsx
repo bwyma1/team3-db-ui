4e4623d56b83dfd56c1a53d038c5526d7039bc8d
Hasan Zalkout <hasan.zalkout@gmail.com>
2023-04-20 01:25:26 -0500

    basic homepage

--- frontend/src/Views/Homepage.jsx
+++ frontend/src/Views/Homepage.jsx
@@ -1,24 +1,51 @@
-import { Box, Container } from "@mui/material";
+import React from 'react';
+import { Box, Container, Typography, Button, Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Homepage() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    navigate.push(path);
+  };
+
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="lg">
       <Box
         sx={{
           marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
-        <iframe
-          width={700}
-          height={500}
-          title="rick"
-          id="rick"
-          src="https://www.youtube.com/embed/V-_O7nl0Ii0"
-        ></iframe>
+        <Typography component="h1" variant="h2">
+          Welcome to Truck Rental
+        </Typography>
+        <Typography component="p" variant="h6" sx={{ mt: 2, textAlign: 'center' }}>
+          Your one-stop solution for renting trucks of various makes, models, and sizes. Choose from our wide range of trucks and enjoy seamless rental experience.
+        </Typography>
+        <Grid container spacing={2} sx={{ mt: 4 }}>
+          <Grid item xs={12} md={6}>
+            <Typography component="h3" variant="h5" fontWeight="bold">
+              Why choose us?
+            </Typography>
+            <Typography component="p" variant="body1" sx={{ mt: 1 }}>
+              - Wide range of truck models and sizes
+            </Typography>
+            <Typography component="p" variant="body1">
+              - Competitive pricing and discounts
+            </Typography>
+            <Typography component="p" variant="body1">
+              - Convenient locations and availability
+            </Typography>
+            <Typography component="p" variant="body1">
+              - Secure and hassle-free rental process
+            </Typography>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
 }
+
