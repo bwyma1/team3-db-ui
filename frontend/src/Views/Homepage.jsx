93cd205859fb972328a3538d07645d8207931f44
Hasan Zalkout <hasan.zalkout@gmail.com>
2023-04-20 03:10:10 -0500

    homepage styliing

--- frontend/src/Views/Homepage.jsx
+++ frontend/src/Views/Homepage.jsx
@@ -22,46 +22,46 @@ export default function Homepage() {
         }}
       >
         <Paper sx={{ p: 4, borderRadius: 2, backgroundColor: '#fff' }}>
-          <Typography component="h1" variant="h2">
-            Welcome to Truck Bros.
+          <Typography component="h1" variant="h1" sx={{textAlign: "center"}}>
+            Welcome to Truck Bros. Rental!
           </Typography>
-          <Typography component="p" variant="h6" sx={{ mt: 2, textAlign: 'center' }}>
+          <Typography component="p" variant="h5" sx={{ mt: 2, textAlign: 'center' }}>
             Your one-stop online website for renting trucks of various makes, models, and sizes. Choose from our wide range of trucks and enjoy a seamless rental experience.
           </Typography>
-          <Typography component="p" variant="h6" sx={{ mt: 2, textAlign: 'center' }}>
+          <Typography component="p" variant="h5" sx={{ mt: 2, textAlign: 'center' }}>
             Interested in listing a truck? Create an account and list your truck(s) individually or in a bundle today!
           </Typography>
           <Grid container spacing={2} sx={{ mt: 4 }}>
             <Grid item xs={12} md={6}>
-              <Typography component="h3" variant="h5" fontWeight="bold">
+              <Typography component="h3" variant="h3" fontWeight="bold">
                 Why choose us?
               </Typography>
-              <Typography component="p" variant="body1" sx={{ mt: 1 }}>
+              <Typography component="p" variant="body1" sx={{ mt: 1, fontSize: "1.5rem" }}>
                 - Wide range of truck models and sizes
               </Typography>
-              <Typography component="p" variant="body1">
+              <Typography component="p" variant="body1" sx={{fontSize: "1.5rem"}}>
                 - Competitive pricing and discounts
               </Typography>
-              <Typography component="p" variant="body1">
+              <Typography component="p" variant="body" sx={{fontSize: "1.5rem"}}>
                 - Convenient locations and availability
               </Typography>
-              <Typography component="p" variant="body1">
+              <Typography component="p" variant="body1" sx={{fontSize: "1.5rem"}}>
                 - Secure and hassle-free rental process
               </Typography>
             </Grid>
             {context.currUser == null ? (
-              <Grid item xs={10} md={2}>
+              <Grid item xs={8} md={2}>
                 <Button
                   variant="contained"
                   onClick={() => handleNavigate('/signup')}
-                  sx={{ mt: 2, backgroundColor: '1976d2', borderColor: '#1976d2', color: '#fff' }}
+                  sx={{ mt: 2, backgroundColor: '1976d2', borderColor: '#1976d2', color: '#fff', width: '20rem', mb: 2 }}
                 >
                   Create an Account Right Now!
                 </Button>
                 <Button
                   variant="contained"
                   onClick={() => handleNavigate('/login')}
-                  sx={{ mt: 2, backgroundColor: '#1976d2', borderColor: '#1976d2', color: '#fff' }}
+                  sx={{ mt: 2, backgroundColor: '#1976d2', borderColor: '#1976d2', color: '#fff', width: '20rem' }}
                 >
                   Already Have an Account with us?
                   Log in!
